6d3b03232f99b5e1cb793d4f84959884ff5eb127
Mason J. Katz <Mason.Katz@gmail.com>
2019-02-26 10:55:52 -0800

    use figlet

--- start.sh
+++ start.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 
-cat << 'EOF'
-
-
-
-   ___                ______           
-  |_  |               | ___ \          
-    | | __ _ _ __ ___ | |_/ / _____  __
-    | |/ _` | '_ ` _ \| ___ \/ _ \ \/ /
-/\__/ / (_| | | | | | | |_/ / (_) >  < 
-\____/ \__,_|_| |_| |_\____/ \___/_/\_\
-
-
-
-EOF
-
-
-runuser -l jam -c "ssh-keygen -t rsa -f ~/.ssh/id_rsa -P ''"
+echo
+echo
+echo
+figlet JamBox
+echo
+echo
+echo
 
 ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N ''
 /usr/sbin/sshd -D
